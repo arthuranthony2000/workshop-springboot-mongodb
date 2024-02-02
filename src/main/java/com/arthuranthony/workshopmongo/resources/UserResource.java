cd11ee34f10198b633b870d7d6217253c3df8ead
Arthur Anthony <arthuranthony2000@gmail.com>
2024-02-02 18:38:02 -0300

    adjust UserResource.java archive

--- src/main/java/com/arthuranthony/workshopmongo/resources/UserResource.java
+++ src/main/java/com/arthuranthony/workshopmongo/resources/UserResource.java
@@ -1,8 +1,12 @@
 package com.arthuranthony.workshopmongo.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arthuranthony.workshopmongo.domain.User;
@@ -10,7 +14,13 @@ import com.arthuranthony.workshopmongo.domain.User;
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
-	public List<User> findAll(){
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
 		User maria = new User("1", "Maria Silva", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<User>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
 	}
 }
