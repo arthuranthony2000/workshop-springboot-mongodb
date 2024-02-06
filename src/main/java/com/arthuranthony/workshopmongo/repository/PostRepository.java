5f3c03e8640d042d419a3903620d84580ff2102d
Arthur Anthony <arthuranthony2000@gmail.com>
2024-02-06 20:54:28 -0300

    add post entity

---
+++ src/main/java/com/arthuranthony/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.arthuranthony.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthuranthony.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
